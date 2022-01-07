817b90054971aec1136a3d6a1b5e4796f9cbb14c
Tofpu <47629321+Tofpu@users.noreply.github.com>
2022-01-07 21:32:12 +0200

    added next-seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
+import {DefaultSeo} from "next-seo";
 
 function MyApp({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <DefaultSeo
+                openGraph={{
+                    type: 'website',
+                    locale: 'en_IE',
+                    url: 'https://tofpu.me',
+                    site_name: 'A portfolio site for Tofpu',
+                    images: [{
+                        url: 'https://tofpu.me/preview.png',
+                        alt: 'preview of the site',
+                        type: 'image/png'
+                    }]
+                }}
+
+                twitter={{
+                    handle: '@Tofpu',
+                    site: 'https://tofpu.me'
+                }}
+            />
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp

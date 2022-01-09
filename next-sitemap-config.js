eaa1a37085803fff8eed588417ae74c1ed171ffa
Tofpu <47629321+Tofpu@users.noreply.github.com>
2022-01-09 22:44:43 +0200

    wrong domain

--- next-sitemap-config.js
+++ next-sitemap-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://example.com',
+    siteUrl: process.env.SITE_URL || 'https://tofpu.me',
     generateRobotsTxt: true, // (optional)
     // ...other options
 }

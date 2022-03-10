2aa855d5b72f90b563074923ab45f34870a4624a
Tofpu <47629321+Tofpu@users.noreply.github.com>
2022-03-10 14:42:02 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/rjks6D5Ynq',
+        permanent: true,
+      },
+    ]
+  },
+}
